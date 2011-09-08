20351f86eabcc4ab7be6d98f6d9894740ff7f8df
EvoTech <iv_zak@yahoo.com>
2011-09-08 23:38:02 +0300

    i18n fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,20 @@
 class ApplicationController < ActionController::Base
+  include ControllerAuthentication
   protect_from_forgery
+  
+  before_filter :set_locale
+ 
+  def set_locale
+    I18n.locale = params[:locale] #extract_locale_from_tld
+  end
+ 
+  def extract_locale_from_tld
+    parsed_locale = params[:locale]
+    I18n.available_locales.include?(parsed_locale.to_sym) ? parsed_locale  : nil
+  end
+  
+  def default_url_options(options={})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
 end
